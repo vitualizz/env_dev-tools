2500392a03db573155b5caa550eeee3cba761d1c
Lee Palacios <vitualizz@gmail.com>
2019-10-17 19:56:28 -0500

    Update truncate_all_tables_sperant.rb

--- scripts/sperant/truncate_all_tables_sperant.rb
+++ scripts/sperant/truncate_all_tables_sperant.rb
@@ -2,8 +2,10 @@ Account.all.each do |account|
   puts "select #{account.id} (#{account.name})"
   ActiveRecord::Base.connection.schema_search_path = "#{account.schema}, hstore"
   ActiveRecord::Base.connection.tables.each do |table|
-    next if table.match(/\Aschema_migrations\Z/)     
-    puts "#{table}"
-    ActiveRecord::Base.connection.execute("DELETE FROM #{table};")
+    begin
+      ActiveRecord::Base.connection.execute("ALTER TABLE #{table} ADD PRIMARY KEY (id)")
+    rescue
+      next
+    end
   end
 end
