ed11017144fc354b7b60a2679c41e21989daa8e0
Lee Palacios <vitualizz@gmail.com>
2020-05-27 02:30:17 -0500

    fix bug

--- resources/tools/nvim/install.sh
+++ resources/tools/nvim/install.sh
@@ -5,4 +5,4 @@ apt install -y neovim
 
 echo "Install Plugins for NeoVim"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp -r ./config ~/.config/vim
+cp -r ./config ~/.config/nvim
